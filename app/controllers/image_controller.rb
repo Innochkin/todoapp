6e97b95b14acd3565c12a88c271e3dbbd07f5bfd
Inna Vasserman <innochkin@gmail.com>
2018-05-10 19:23:42 +0000

    add edit action

---
+++ app/controllers/image_controller.rb
@@ -0,0 +1,6 @@
+class ImagesController < ApplicationController
+  def display_img
+    @img =Image.find (params [:id])
+    send_data(@img.img, :type => @img.filetype, :filename =>@img.filename, :disposition => 'inline')
+  end
+end
